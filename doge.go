9cc46ddcd489c1bc0bd644827a068c538d0dcae7
davidjoliver86 <davidjoliver.2021@gmail.com>
2024-01-15 09:40:33 -0800

    third

--- doge.go
+++ doge.go
@@ -1,6 +1,10 @@
 package doge
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/davidjoliver86/shiba"
+)
 
 func Bark() string {
 	return "BARK"
@@ -8,4 +12,12 @@ func Bark() string {
 
 func Such(thing string) string {
 	return "such " + strings.ToLower(thing)
+}
+
+func BigBark() string {
+	return shiba.WhenGrownUp(Bark())
+}
+
+func BigSuch(thing string) string {
+	return shiba.WhenGrownUp(Such(thing))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/davidjoliver86/doge
 
 go 1.21.5
+
+require github.com/davidjoliver86/shiba v0.0.0-20240115072545-22a6d878b1a2 // indirect
